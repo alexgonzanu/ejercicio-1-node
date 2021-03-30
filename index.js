35b1a859ef47dfda33700e8b4782a64de195b02e
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-03-30 21:48:44 +0200

    arreglado el error cuando no le pasabas parametro

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const chalk = require("chalk");
 const path = require("path");
 const saludo = require("./utilidades/archivo");
 
-if (process.argv.slice(2)) {
+if (process.argv[2]) {
   saludo(process.argv[2].split("=")[1]);
 } else {
   console.log(chalk.yellow("No has escrito ningun parametro en la consola"));
