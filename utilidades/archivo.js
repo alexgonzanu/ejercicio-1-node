7f2c0551fac7e1be090d92bd13dab7ac3a79d145
Alejandro Gonzalez <alexgonzanu@gmail.com>
2021-03-30 17:33:15 +0200

    exportada la funcion guardarSaludo

--- utilidades/archivo.js
+++ utilidades/archivo.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
+const chalk = require("chalk");
 
 const guardarSaludo = (nombre) => {
-  fs.writeFile("saludo.txt", `Hola, ${nombre}. Encantado de conocerte`, (err) => {
+  fs.writeFile("saludosf.tsdfvsdfdsfdsxt", `Hola, ${nombre}. Encantado de conocerte`, (err) => {
     if (err) {
-      return console.log("No se ha podido escribir el archivo");
+      console.log(chalk.bold.red("No se ha podido escribir el archivo"));
+      process.exit(1);
     }
     console.log("La escritura se ha hecho correctamente");
   });
 };
+
+module.exports = guardarSaludo;
